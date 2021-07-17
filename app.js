4ec084b7a02911101267491010fcfdc6d54592e8
sik2 <devpang20@gmail.com>
2021-07-17 22:41:44 +0900

    feat: add event handler

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 const canvas = document.getElementById("jsCanvas");
 
-function onMouseMove(event) {
-    console.log(event);
+let painting = false;
+
+function stopPainting() {
+    painting = false;
+}
+
+function onMouseMove(e) {
+    const x = e.offestX;
+    const y = e.offestY;
+}
+
+function onMouseDown(e) {
+    painting = true;
+}
+
+//@todo edit
+function onMouseUp(e) {
+    stopPainting();
 }
 
 if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mousedown", onMouseDown);
+    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mouseleave", stopPainting);
 }
